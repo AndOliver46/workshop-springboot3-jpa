9bc984db089324a065607bdc5f13275ee26ba571
AndOliver46 <anderson_souza75@live.com>
2022-10-13 16:03:46 -0300

    User entity and resource

---
+++ src/main/java/com/andoliver46/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.andoliver46.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.andoliver46.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "Maria@gmail.com", "11941416022", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
