5c6af36f4f0066c421bd8f1feafaa519e6a5b86d
AndOliver46 <anderson_souza75@live.com>
2022-10-13 09:40:53 -0300

    Project created

---
+++ src/main/java/com/andoliver46/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.andoliver46.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
