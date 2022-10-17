61d6f98dccfced3faeb7281f6055c3a99f336c06
AndOliver46 <anderson_souza75@live.com>
2022-10-17 12:47:07 -0300

    Service layer, component registration

--- src/main/java/com/andoliver46/course/repositories/UserRepository.java
+++ src/main/java/com/andoliver46/course/repositories/UserRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.andoliver46.course.entities.User;
 
+//Não precisa da annotation @Repository pois já herdou da classe extendida JpaRepository
 public interface UserRepository extends JpaRepository<User,Long>{
 }
