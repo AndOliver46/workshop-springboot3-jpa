d2ae80b4474c678d7aaca9c08a08e88b902e59c2
AndOliver46 <anderson_souza75@live.com>
2022-10-19 16:31:00 -0300

    Product

---
+++ src/main/java/com/andoliver46/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.andoliver46.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.andoliver46.course.entities.Category;
+
+//Não precisa da annotation @Repository pois já herdou da classe extendida JpaRepository
+public interface CategoryRepository extends JpaRepository<Category,Long>{
+}
---
+++ src/main/java/com/andoliver46/course/services/ProductService.java
@@ -0,0 +1,28 @@
+package com.andoliver46.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andoliver46.course.entities.Product;
+import com.andoliver46.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+		
+	}
+
+}
